901dafb6539492c1cc3fde5129e8e4f18e75b6bb
幻象Phantoms <82647748+Phantomxm2021@users.noreply.github.com>
2021-05-13 13:32:14 +0800

    Add files via upload

---
+++ FaceMeshExample/Scripts/Runtime/FaceMeshExampleMainEntry.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using com.Phantoms.ActionNotification.Runtime;
+using com.Phantoms.ARMODAPI.Runtime;
+
+namespace FaceMeshExample
+{
+    public class FaceMeshExampleMainEntry
+    {
+        private API api;
+        private GameObject faceMesh;
+        private Transform faceMeshTransform;
+
+        //Please delete the function if it is not used
+        public void OnLoad()
+        {
+            //Use this for initialization
+            api = new API();
+        }
+    }
+}
